cdd17734ab8cf116dcf4c149dfd3545c4a4b1e11
Nicky Hendrik Sen <nicky.sen@binus.edu>
2021-06-14 11:11:56 +0700

    Added Router

---
+++ src/views/login/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Login">
+    <div class="LoginHeader">
+      <div class="HeaderTitle">Gym Pod</div>
+    </div>
+    <div class="LoginContent">
+      <div class="LoginContentWrapper">
+        <div class="LoginTitle">Login</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  @import "./Login.scss";
+</style>
